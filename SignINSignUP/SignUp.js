6d41d85ce2fdce1ccd81d7070c0539b3bd4f7381
Dang Quang Vi <vidangquang1203@gmail.com>
2023-05-15 23:18:05 +0700

    update SignUP
    Chưa hoàn thiện phần planet

--- SignINSignUP/SignUp.js
+++ SignINSignUP/SignUp.js
@@ -1,3 +1,4 @@
+//Ẩn hiện password
 function togglePassword() {
     var password = document.getElementsByName("password")[ 0 ];
     if (password.type === "password") {
@@ -5,8 +6,7 @@ function togglePassword() {
     } else {
         password.type = "password";
     }
-}
-function toggleCPassword() {
+} function togglePassword1() {
     var password = document.getElementsByName("password")[ 1 ];
     if (password.type === "password") {
         password.type = "text";
@@ -15,117 +15,187 @@ function toggleCPassword() {
     }
 }
 
-const imageUpload = document.getElementById('image-upload');
-const imageUploadLabel = document.querySelector('label[for="image-upload"]');
-
-function showINdisplay(message) {
+// Notification
+function displayError(message) {
     // Hiển thị thông báo lỗi trong một cửa sổ nhỏ
     alert(message);
 }
 
-function emailchecked(text) {
-    const testEmail = /^[a-zA-Z0-9._]+@+[a-zA-Z0-9-]+\.[a-zA-Z.-]{2,}$/;
+const emailElemet = document.getElementById("email");
+const usernameElement = document.getElementById("username");
+// const sexElement = document.querySelector("input[name=='option']:checked");
+const passwordElement = document.getElementById("password");
+const passConfiElement = document.getElementById("passwordConfirm");
+const dOBElement = document.getElementById("dOB");
+const submitElement = document.getElementById("submit");
 
-    return testEmail.test(text);
+console.log(emailElemet.value);
+console.log(usernameElement.value);
+// console.log(sexElement.value);
+console.log(passwordElement.value);
+console.log(passConfiElement.value);
+console.log(passwordElement.value);
+console.log(dOBElement.value);
+
+const imf_List = [
+    {
+        div: ".divEmail",
+        errorID: "#errorEmail",
+        error: /^[a-zA-Z0-9._]+@+[a-zA-Z0-9-]+\.[a-zA-Z.-]{2,}$/,
+        errorText: "Không thể xác định email của bạn!"
+    },
+    {
+        div: ".divusername",
+        errorID: "#errorUsername",
+        error_number: /^.{6,}$/,
+        error_numberText: "Tối thiểu 6 ký tự !"
+    },
+    {
+        div: ".divpassword",
+        errorID: "#errorPassword",
+        error_number: /^.{6,}$/,
+        error_numberText: "Tối thiểu 6 ký tự !",
+        error: /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/,
+        errorText: "Tối thiểu 1 chữ IN hoa, 1 ký tự in thường !",
+        error_1: /^(?=.*\d)(?=.*[!@#$%^&*]).{6,}$/,
+        errorText_1: "Mật khẩu cần phải có 1 chữ số và 1 ký tự đặc biệt [!@#$%^&*]!"
+    },
+    {
+        div: ".divConfirm",
+        errorID: "#errorConfirmpassword",
+        error: passConfiElement.value === passwordElement.value,
+        errorText: "Mật khẩu nhập lại không khớp!"
+    },
+    {
+        errorID: "#errorPlanet",
+        errorText: "Vui lòng điền mục này!"
+    },
+    {
+        div: ".divdOB",
+        errorID: "#errordOB",
+        error: new Date(dOBElement.value).getFullYear() < 1900,
+        errorText: "Bạn đã sống trên 123 tuổi?\nNhập lại năm sinh!\nRe-enter birthday"
+    }
+];
+
+// Data final input
+const accerp_list = {
+    email: null,
+    username: null,
+    password: null,
+    sex: null,
+    dOB: null
+};
+
+// validate function
+function listenerElement(imflist) {
+    const { name, value } = event.target;
+    const divEvent = document.querySelector(imflist.div);
+    const pError = divEvent.querySelector(imflist.errorID);
+
+    let value_ = value.trim();
+
+    if (!value_) {
+        pError.innerText = "Vui lòng nhập thông tin này";
+    } else if (imflist.error_number != undefined && !imflist.error_number.test(value_)) {
+        pError.innerText = imflist.error_numberText;
+    } else if (imflist.error != undefined && !imflist.error.test(value_)) {
+        pError.innerText = imflist.errorText;
+    } else if (imflist.error_1 != undefined && !imflist.error_1.test(value_)) {
+        pError.innerText = imflist.errorText_1;
+    } else {
+        const final = value_;
+        pError.innerText = "";
+        console.log("accept!");
+        return final;
+    }
 }
-function checkPassword(password) {
-    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{6,}$/;
-    return regex.test(password);
-}
 
-// Lấy dữ diệu từ INPUT
-let email = document.getElementsByTagName('input')[ 0 ];
+const email_list = imf_List[ 0 ];
+const user_list = imf_List[ 1 ];
+const pass_list = imf_List[ 2 ];
+const passConfirm_list = imf_List[ 3 ];
+const sex_list = imf_List[ 4 ];
+const dOB_list = imf_List[ 5 ];
 
-let username = document.getElementsByTagName('input')[ 1 ];
+const check_list = {};
 
-let planet = document.querySelector('input[name="option"]:checked');
+emailElemet.addEventListener("keyup", (event) => {
+    accerp_list.email = (listenerElement(email_list));
+});
 
-let password = document.getElementsByTagName('input')[ 4 ];
+usernameElement.addEventListener("keyup", (event) => {
+    accerp_list.username = (listenerElement(user_list));
+});
 
-let confirmpassword = document.getElementsByTagName('input')[ 5 ];
+passwordElement.addEventListener("keyup", (event) => {
+    accerp_list.password = (listenerElement(pass_list));
+});
 
-let dOB = document.getElementsByTagName('input')[ 6 ];
-let dateINP = new Date(dOB).getTime();
+passConfiElement.addEventListener("keyup", (event) => {
+    check_list.passwordConfirm = (listenerElement(passConfirm_list));
+});
 
-let avt = document.getElementsByTagName('input')[ 7 ];
+// if (!sexElement.value) {
+//     const pError = document.getElementById(sex_list.errorID);
+//     pError.innerText = sex_list.errorText;
+// } else {
+//     accerp_list.sex = sexElement.value;
+// }
 
-
-// Check entry conditions
-if (!username.value || !email.value || !planet.value || !password.value || !confirmpassword.value || !dOB.value) {
-    showINdisplay("Không giá trị nào được để trống!\nNo value can be left blank!")
-} else {
-
-    // Check email format
-    // if (!emailchecked(toString(email.value))) {
-    if (!emailchecked(email.value)) {
-        console.log(!emailchecked((email.value)), "Error email!");
-        showINdisplay("Your email is not the correct format!")
-    }
-
-    // check username 
-    if (!/^[a-zA-Z0-9._]+$/.test(username.value)) {
-        // if(!/^[a-zA-Z0-9._]$/.test(username.value)){
-        showINdisplay("username không có đấu và không chứa các ký tự đặc biệt");
-    }
-    // check password 
-    // Nếu password nhập lại không giống với password ban đầu thì hiển thị mà hình thông báo nhập sai và yêu cầu nhập lại
-    if (!checkPassword(password.value)) {
-        showINdisplay('Mật khẩu cần có ít nhất 6 ký tự! Trong đó phải có 1 chữ số, 1 chữ IN hoa, 1 ký tự in thường và 1 ký tự đặc biệt [!@#$%^&*]!');
-    }
-    if (password.value != confirmpassword.value) {
-        showINdisplay("Confirm the Password is not the same as the previous password \n Please, re-enter your confirm password!")
-    }
-
-    // check năm sinh 
-    // date.year > 1900;
-    console.log(new Date(dOB.value).getFullYear());
-    if (new Date(dOB.value).getFullYear() < 1900) {
-        showINdisplay("Bạn đã sống trên 123 tuổi?\nNhập lại năm sinh!\nRe-enter birthday")
-    }
+dOBElement.addEventListener("keyup", (event) => {
+    const dOB_int = new Date(listenerElement(dOB_list, accerp_list[ 4 ])).getTime;
+    accerp_list.dOB = dOB_int;
+});
 
 
-    var dataSignUp = {
-        username: username.value,
-        email: email.value,
-        password: password.value,
-        sex: planet.value,
-        dOB: dateINP,
-    }
-}
 
-// Check entry values
-console.log('Email: ', email.value, email.type);
-console.log('Username: ', username.value, username.type);
-console.log('Sex: ', planet.value, typeof (planet.value));
-console.log('Password: ', password.value, password.type);
-console.log('Confirm Password: ', confirmpassword.value, confirmpassword.type);
-console.log('Birthday: ', dOB.value, dOB.type);
-console.log('IMG_name: ', avt.value, avt.type);
+api_login = "http://3.95.239.60:9001/api/auth/sign-in";
 
+console.log(accerp_list);
+console.log(check_list);
 
-function savedataRegister() {
-    // POST API
-    async function postJSONSignUP(data) {
-        try {
-            const reponse = await fetch("http://3.95.239.60:9001/api/auth/sign-in", {
+// Hàm POST API có dùng lại
+async function postLoginData(api_url, data) {
+    try {
+        const reponse = await fetch(api_url,
+            {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({
-                    data
-                }),
+                body: JSON.stringify(data),
             });
-            const result = await reponse.json();
-            console.log("Success: ", result);
-            if (reponse.ok) {
-                showINdisplay("Chưa xong");
-            } else {
-                showINdisplay(result.message);
-            }
-        } catch (error) {
-            console.log("Error: ", error);
+
+        const resuft = await reponse.json();
+        if (reponse.ok) {
+            displayError("Thành công!");
+        } else {
+            displayError(resuft.message)
         }
+    } catch (error) {
+        console.log("Error: ", error);
     }
-    postJSONSignUP(dataSignUP);
+};
+
+// Check null các phần tử
+if (Object.values(accerp_list).includes()) {
 }
+submitElement.addEventListener("click", () => {
+
+
+    console.log(emailElemet.value);
+    console.log(usernameElement.value);
+    // console.log(sexElement.value);
+    console.log(passwordElement.value);
+    console.log(passConfiElement.value);
+    console.log(passwordElement.value);
+    console.log(dOBElement.value);
+
+
+    if (Object.values(accerp_list).some(value => !value) && Object.values(check_list).some(value => !value)) {
+        displayError("Vui lòng nhập đầy đủ thông tin");
+    } else {
+        // postLoginData(api_login, accerp_list);
+    }
+});
