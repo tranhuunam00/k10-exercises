d9bc5ea315dd850b327e2407444c484ed0721d95
Khương Nguyễn Đa <dakhuong281202@gmail.com>
2023-07-05 10:24:55 +0700

    language

--- src/components/multiLanguage/i18next/i18next.js
+++ src/components/multiLanguage/i18next/i18next.js
@@ -4,11 +4,7 @@ import English from "../language/eng.json";
 import Vietnamese from "../language/vie.json";
 import Russian from "../language/rus.json";
 
-export const locales = {
-    eng : "English",
-    vie : "Vietnamese",
-    rus : "Russian"
-}
+
 
 const resources = {
     eng: {
@@ -26,7 +22,7 @@ i18n
     .use(initReactI18next) // cho phép dùng thư viện ngoài gài vào i18n, chuyển i18n thành react-i18next
     .init({ // mặc định của i18n sẵn sàng được khởi tạo bằng cách gọi init
         resources, //khởi tạo
-        lng: "eng", // ngôn ngữ sử dụng (tính năng ghi đè nhau)
+        lng:localStorage.getItem("languageValue") || "eng", // ngôn ngữ sử dụng (tính năng ghi đè nhau)
         keySeparator: false,
         interpolation: {
             escapeValue: false,
--- src/modules/users/checkLanguage/userLanguage.jsx
+++ src/modules/users/checkLanguage/userLanguage.jsx
@@ -3,8 +3,8 @@ import { useTranslation } from 'react-i18next';
 import styles from "./styles.module.scss"
 import ButtonCustom from '../../../components/button/ButtonCustom';
 import { useNavigate } from 'react-router';
-import { locales } from '../../../components/multiLanguage/i18next/i18next';
 import { useContext } from "react";
+import { locales } from '../../../components/multiLanguage/i18next/i18next';
 import Context from "../../../store/context";
 
 const UserLanguage = () => {
@@ -18,16 +18,16 @@ const UserLanguage = () => {
     const changeLanguage = (e) => {
         const languageValue = e.target.value // lấy value của thẻ select
         i18n.changeLanguage(languageValue); // sau khi lấy được value sẽ gửi về i18n để xử lí
+        localStorage.setItem("languageValue", languageValue)
     }
-    const currentLanguage = locales[i18n.language]
+    const language = localStorage.getItem("languageValue")
     return (
         <div className={styles.content}>
             <h1>{t("title")}</h1>
-            <select onChange={changeLanguage} >
-                <option value="" > -- {currentLanguage} --</option>
-                <option value="eng">English</option>
-                <option value="vie">Vietnamese</option>
-                <option value="rus">Russian</option>
+            <select onChange={changeLanguage}>
+                <option selected={language === "eng" ? true : false} value="eng">English</option>
+                <option selected={language === "vie" ? true : false} value="vie">Vietnamese</option>
+                <option selected={language === "rus" ? true : false} value="rus">Russian</option>
             </select>
             <ButtonCustom type={"button"} text={"Back"} onClick={handleOnClick} />
         </div>
