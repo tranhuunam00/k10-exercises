d69a7e610c9476baf206cddd099b7c2eb0ca9af5
namTh <tranhuunam23022000@gmail.com>
2023-07-09 16:22:00 +0700

    change api

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:9001/api", // Thay đổi đường dẫn tới API của bạn
+  baseURL: "http://3.85.3.86:9001/api", // Thay đổi đường dẫn tới API của bạn
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
   },
